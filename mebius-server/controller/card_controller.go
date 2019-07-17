29a593eb1844c7ea6256c5b10476694ecc05845e
gut <yuta1402souja@live.jp>
2019-07-18 01:17:04 +0900

    Remove some dead code

--- mebius-server/controller/card_controller.go
+++ mebius-server/controller/card_controller.go
@@ -10,34 +10,8 @@ import (
 
 type CardController struct{}
 
-// This is for mock.
-var card0 entity.Card
-var card1 entity.Card
-
 type UserIndexResponse struct {
 	Cards []entity.Card `json:"cards"`
-}
-
-func init() {
-	// This is for mock.
-	card0 = entity.Card{
-		ID:       0,
-		Name:     "ドラゴンナイト",
-		Point:    4,
-		Species0: "Dragon",
-		Species1: "Human",
-		Job:      "Knight",
-	}
-
-	// This is for mock.
-	card1 = entity.Card{
-		ID:       1,
-		Name:     "デストロイヤー",
-		Point:    5,
-		Species0: "Goblin",
-		Species1: "",
-		Job:      "Mechanic",
-	}
 }
 
 // TODO: Implement the contents
